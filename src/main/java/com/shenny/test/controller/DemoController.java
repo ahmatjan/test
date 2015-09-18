b0c291aaa302806fdff686709a0eb668f8735822
shenny <shenny520@outlook.com>
2015-09-18 14:52:59 +0800

    a

---
+++ src/main/java/com/shenny/test/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.shenny.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName: DemoController
+ * @Description:
+ * @author <a href="mailto:zhonghuagen@qq.com">Huagen_Zhong</a>
+ * @date 2015年9月16日 上午11:10:57
+ */
+
+@Controller
+public class DemoController {
+
+	@RequestMapping("index1")
+	public String index() {
+		return "index";
+	}
+}
---
+++ src/test/java/nos/ConsumerClient.java
@@ -0,0 +1,42 @@
+package nos;
+
+import java.util.Properties;
+
+import com.aliyun.openservices.ons.api.Action;
+import com.aliyun.openservices.ons.api.ConsumeContext;
+import com.aliyun.openservices.ons.api.Consumer;
+import com.aliyun.openservices.ons.api.Message;
+import com.aliyun.openservices.ons.api.MessageListener;
+import com.aliyun.openservices.ons.api.ONSFactory;
+import com.aliyun.openservices.ons.api.PropertyKeyConst;
+
+/**
+ * @ClassName: ConsumerClient
+ * @Description:
+ * @author <a href="mailto:zhonghuagen@qq.com">Huagen_Zhong</a>
+ * @date 2015年9月16日 下午4:59:27
+ */
+public class ConsumerClient {
+
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+		properties.put(PropertyKeyConst.ConsumerId, "CID_15454wd");
+		properties.put(PropertyKeyConst.AccessKey, "DjKWsUrgJGpUeDsO");
+		properties.put(PropertyKeyConst.SecretKey, "8Achu94AEaJHrWGdDCzDUwnnzl3nLS");
+		Consumer consumer = ONSFactory.createConsumer(properties);
+		consumer.subscribe("nos_test_shenny", "*", new MessageListener() {
+
+			@Override
+			public Action consume(Message message, ConsumeContext context) {
+
+				System.err.println(new String(message.getBody()));
+				Action a=Action.ReconsumeLater;
+				System.err.println(a.name());
+				return Action.CommitMessage;
+			}
+
+		});
+		consumer.start();
+	}
+
+}
