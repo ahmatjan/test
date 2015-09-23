b1ffd22b1917a4653e24587218398eee49c2fa36
shenny <shenny520@outlook.com>
2015-09-23 10:18:12 +0800

    s

--- src/main/java/com/shenny/test/controller/DemoController.java
+++ src/main/java/com/shenny/test/controller/DemoController.java
@@ -1,7 +1,14 @@
 package com.shenny.test.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shenny.test.dao.IEcSaleImgDao;
+import com.shenny.test.model.EcSaleImg;
 
 /**
  * @ClassName: DemoController
@@ -12,10 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
+	
+	@Inject
+	IEcSaleImgDao ecSaleImgDao;
 
-	@RequestMapping("index1")
+	@RequestMapping("index")
 	public String index() {
 		
+		List<EcSaleImg> list = ecSaleImgDao.listAllImg();
+		for (EcSaleImg img : list) {
+			System.out.println(img.getId() + "\t" + img.getSaleId() + "\t" + img.getImgUri());
+		}
 		return "index";
 	}
 }
